bfcee55c9865e2f7c227544862697809aaeaee66
Angga Surya Prianto <anggasuryaprianto@gmail.com>
2022-03-04 15:15:29 +0700

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = "1"
+    node_count = "2"
     vm_size    = var.instance_type
   }
   identity {
@@ -33,5 +33,53 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     http_application_routing {
       enabled = true
     }
+  }
+}
+
+data "azurerm_kubernetes_cluster" "credentials" {
+  name                = azurerm_kubernetes_cluster.cluster.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+
+  }
+}
+
+resource "helm_release" "deployment" {
+
+  name = "test-deployment"
+  repository = "https://kubernetes.github.io/dashboard/"
+  chart      = "kubernetes-dashboard"
+  namespace  = "default"
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "protocolHttp"
+    value = "true"
+  }
+
+  set {
+    name  = "service.externalPort"
+    value = 80
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 2
+  }
+
+  set {
+    name  = "rbac.clusterReadOnlyRole"
+    value = "true"
   }
 }
