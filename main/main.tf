ba95e32b4b84406a09f51aea60b74612ffd0c9df
Angga Surya Prianto <anggasuryaprianto@gmail.com>
2022-03-04 23:29:09 +0700

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
         resource_group_name  = "tfstate"
-        storage_account_name = "tfstate72"
+        storage_account_name = "tfstate29347"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
